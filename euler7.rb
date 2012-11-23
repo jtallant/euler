e10aaa970e318969c224f79a172e18ea1411472d
Justin Tallant <jtallant07@gmail.com>
2012-11-22 19:07:25 -0500

    add a few more expectations

--- euler7.rb
+++ euler7.rb
@@ -9,13 +9,14 @@ def is_prime?(n)
 end
 
 def find_prime(target)
-  count = 2
-  index = 0
+
+  count = 3
+  index = 1 # start at 1 since we are skipping the number 2 (the first prime numer)
 
   while true
     index += 1 if is_prime?(count)
     return count if index == target
-    count += 1
+    count += 2 # only test odd numbers
   end
 end
 
@@ -24,7 +25,10 @@ require 'rspec'
 describe :find_prime do
 
   it 'returns the nth prime number' do
+    expect(find_prime(3)).to eq(5)
     expect(find_prime(6)).to eq(13)
+    expect(find_prime(10)).to eq(29)
+    expect(find_prime(10001)).to eq(104743)
   end
 
 end
